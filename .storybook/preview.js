64a3b84c1c1fee71afbd1e3c20fe7529c9de5305
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-05-22 14:43:45 +0530

    feat: update inline container background color

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -22,7 +22,7 @@ export const parameters = {
           style={{
             width: "100%",
             height: "100%",
-            backgroundColor: "#0cf",
+            backgroundColor: "#e2cdf7",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
